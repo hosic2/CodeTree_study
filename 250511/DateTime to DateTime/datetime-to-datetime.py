25d60b27384ff565c66a33d7b8d2b4848b1c5310
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-11 20:45:10 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250511/DateTime to DateTime/datetime-to-datetime.py
+++ 250511/DateTime to DateTime/datetime-to-datetime.py
@@ -1,4 +1,6 @@
 a, b, c = map(int, input().split())
 
-# Please write your code here.
-print((a * 60 * 24 + b * 60 + c) - (11 * 60 * 25 + 11) if a > 11 and b > 11 and c > 11 else -1) 
+base = (11 * 24 * 60) + (11 * 60) + 11
+now = (a * 24 * 60) + (b * 60) + c
+
+print(now - base if now >= base else -1)
